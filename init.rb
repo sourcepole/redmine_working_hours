ab7f167ec834c93ecb77a9293f5235816aba496a
Mathias Walker <mwa@mwa-laptop.(none)>
2009-12-15 13:42:18 +0100

    - Readme update - Version update

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_working_hours do
   name 'Redmine Working Hours plugin'
  author 'Pirmin Kalberer'
   description 'A plugin for working time integration in Redmine.'
-  version '0.1'
+  version '0.2'
 
   menu :account_menu, "Working hours", :controller => 'working_hours', :action => 'index'
 end
